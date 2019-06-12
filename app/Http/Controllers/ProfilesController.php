f83fc501a57b5d5171853f161c4f78de27e57bd8
greatgetsby <realdhawan@gmail.com>
2019-06-12 16:00:51 +0530

    2nd chane

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use app\user;
 
-class ProfilesController extends Controller
+class ProfilesController extends Controller ///////ghvhvhvhvh
 {
     public function index()
     {
